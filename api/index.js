3b98ba027d22ce4b1716ff6873818de71e7c815e
ts-31 <104221666+ts-31@users.noreply.github.com>
2025-07-10 12:29:57 +0530

    Added cors

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import matchRoutes from "./routes/match.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api", matchRoutes);
 
